2ad8b679a89c3e72b47d0dbc385e79a91c2aeae9
clan <ryo.kurosaki@gmail.com>
2016-09-15 17:43:51 +0000

    fixed resist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :name
-      devise_parameter_sanitizer.for(:sign_up) << :position
+      devise_parameter_sanitizer.for(:sign_up) << :position_id
       devise_parameter_sanitizer.for(:account_update) << :name
-      devise_parameter_sanitizer.for(:account_update) << :position
+      devise_parameter_sanitizer.for(:account_update) << :position_id
     end
 end
